c5ac7dcdcad301cbdf6ccb21b48e6e30af1b7f0e
Mahmoud Al Nakawa <alnakawm@sheridancollege.ca>
2024-10-30 18:22:37 -0400

    test commit

---
+++ Projects/Iterprise /interpriseproject/Car_Rental_Service_Portal/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Car_Rental_Service_Portal.Models;
+
+
+namespace Car_Rental_Service_Portal.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        // Action method for the Home page
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Action method for the Privacy page
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        // Action method for displaying profiles and cars
+        public IActionResult ProfilesAndCarsView()
+        {
+            // Sample data
+            var profiles = new List<Profile>
+        {
+            new Profile { Username = "user1", IsAdmin = true },
+            new Profile { Username = "user2", IsAdmin = false }
+        };
+
+            var cars = new List<Car>
+        {
+            new Car { CarId = 1, Brand = "Tesla", Year = 2020, IsElectric = true },
+            new Car { CarId = 2, Brand = "Toyota", Year = 2018, IsElectric = false }
+        };
+
+            var viewModel = new ProfileCarViewModel
+            {
+                Profiles = profiles,
+                Cars = cars
+            };
+
+            return View(viewModel);
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
+   
